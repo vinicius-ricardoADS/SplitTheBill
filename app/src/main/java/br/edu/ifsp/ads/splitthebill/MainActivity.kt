6308a6879c562c0e5d84068876ec8dee90e134d6
Vinicius Noli <viniciusnoli2016@gmail.com>
2023-05-02 17:06:34 -0300

    first commit

---
+++ app/src/main/java/br/edu/ifsp/ads/splitthebill/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.edu.ifsp.ads.splitthebill
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
